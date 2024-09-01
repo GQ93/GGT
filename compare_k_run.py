024b3ae8d6ce7b5aa2a0c99fad9c1bf182bbcd7a
GQ93 <gqu1@tulane.edu>
2024-09-01 00:47:23 -0500

    v 2

--- compare_k_run.py
+++ compare_k_run.py
@@ -14,9 +14,9 @@ for paradigm in paradigms_hcp:
             p=paradigm, e=int(5), s=k_i)
         os.system(cmd)
 
-# for paradigm in paradigms_pnc:
-#     for cnb_score in cnb_scores:
-#         for k_i in k:
-#             cmd = 'set CUDA_VISIBLE_DEVICES=0 &python main_PNC_multiregression.py --paradigms {p} --max_epochs {e} --cnb_scores {c} --sparse {s}'.format(
-#                 p=paradigm, e=int(5 ), c=cnb_score, s=k_i)
-#             os.system(cmd)
+for paradigm in paradigms_pnc:
+    for cnb_score in cnb_scores:
+        for k_i in k:
+            cmd = 'set CUDA_VISIBLE_DEVICES=0 &python main_PNC_multiregression.py --paradigms {p} --max_epochs {e} --cnb_scores {c} --sparse {s}'.format(
+                p=paradigm, e=int(5 ), c=cnb_score, s=k_i)
+            os.system(cmd)
